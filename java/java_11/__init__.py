6809f4f7432018fdb92cc9f8e121d98052754e5b
Joss Whittle <joss.whittle@gmail.com>
2020-05-27 15:18:09 +0100

    Configure Java 11

---
+++ java/java_11/__init__.py
@@ -0,0 +1,26 @@
+def configure(verbose=False):
+  import os
+  from csswansea.utils import execute_command
+
+  print()
+  print('installing java')
+  install_output = execute_command(f'apt-get install -y openjdk-11-jdk-headless')
+  if verbose:
+    print(install_output)
+
+  print()
+  print('setting JAVA_HOME env variable')
+  JAVA_HOME = '/usr/lib/jvm/java-11-openjdk-amd64'
+  os.environ['JAVA_HOME'] = JAVA_HOME                             
+  
+  print()
+  print('updating-alternatives to use java version')
+  print(execute_command(f'update-alternatives --set java    {JAVA_HOME}/bin/java'))
+  print(execute_command(f'update-alternatives --set javac   {JAVA_HOME}/bin/javac'   ))
+  print(execute_command(f'update-alternatives --set javadoc {JAVA_HOME}/bin/javadoc' ))
+  
+  print()
+  print('java version')
+  print(execute_command(f'java -version'))
+  print('javac version')
+  print(execute_command(f'javac -version'))
