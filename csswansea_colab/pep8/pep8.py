24d54de02886ef68b16764b616231ff67e6f8c28
Joss Whittle <joss.whittle@gmail.com>
2020-05-27 23:27:44 +0100

    Added pep8

---
+++ csswansea_colab/pep8/pep8.py
@@ -0,0 +1,61 @@
+import os, tempfile, shutil
+import csswansea_colab
+from csswansea_colab.utils import execute_command, execute_interactive_command
+
+def configure(verbose=False, execute_tests=True):
+  print()
+  print('installing pep8 from source')
+  execute_command(f'mkdir -p /content/pep8', verbose=verbose)
+  shutil.rmtree('/content/pep8/')
+  execute_command(f'unzip {os.path.dirname(__file__)}/pep8.zip -d /content/', verbose=verbose)
+  execute_command(f'make -C /content/pep8/', verbose=verbose)
+  
+  print()
+  print('updating path env variable')
+  os.environ['PATH'] += ':/content/pep8/'  
+
+  print()
+  print('updating-alternatives to use pep8')
+  execute_command(f'update-alternatives --install /usr/bin/asem8 asem8 /content/pep8/asem8 1', verbose=verbose)
+  execute_command(f'update-alternatives --install /usr/bin/pep8  pep8  /content/pep8/pep8  1', verbose=verbose)
+
+  print()
+  print('building pep8os')
+  execute_command(f'asem8 /content/pep8/pep8os.pep', verbose=verbose)
+  
+  print()
+  print('pep8 version')
+  execute_command(f'asem8 -v', verbose=True)
+  execute_interactive_command(f'pep8 -v', ['q'], verbose=True)
+
+  if execute_tests:
+    test(verbose=verbose)
+
+def test(verbose=False):
+  print()
+  print('executing tests')
+    
+  tmpdir = tempfile.mkdtemp()
+  
+  with open(f'{tmpdir}/HelloWorld.pep', 'w') as f:
+    f.write("""br main
+
+msg: .ASCII "Hello World.\x00"
+
+main:    STRO msg, d 
+         STOP
+
+.END """)
+
+  execute_command(f'asem8 {tmpdir}/HelloWorld.pep', verbose=verbose)
+  execute_interactive_command(f'pep8', ['l', f'{tmpdir}/HelloWorld', 'x', 'o', 'f', f'{tmpdir}/HelloWorld.out', 'x', 'q'], verbose=False)
+
+  with open(f'{tmpdir}/HelloWorld.out', 'r') as f:
+    result = f.read()
+
+  if 'Hello World.' not in result:
+    raise Exception('pep8 HelloWorld test did not produce the expected output...')
+  else:
+    print('Tests passed.')
+
+  shutil.rmtree(tmpdir)
--- csswansea_colab/utils/__init__.py
+++ csswansea_colab/utils/__init__.py
@@ -1,7 +1,7 @@
+import subprocess
 
 def execute_command(cmd, verbose=False):
-  import re, subprocess
-
+  
   print('++', cmd)
 
   prc       = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -10,5 +10,24 @@ def execute_command(cmd, verbose=False):
 
   if verbose:
     print(stdout)  
+
+  return stdout
+
+def execute_interactive_command(cmd, icmds, verbose=False):
+
+  print('++', cmd)
+  prc = subprocess.Popen(cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  
+  for icmd in icmds:
+    print(f'++ {icmd.strip()}')
+    
+    prc.stdin.write(f'{icmd.strip()}\n'.encode('ascii'))
+    prc.stdin.flush()
+  
+  stdout, _ = prc.communicate()
+  stdout    = stdout.decode('ascii')
+
+  if verbose:
+    print(stdout) 
 
   return stdout
